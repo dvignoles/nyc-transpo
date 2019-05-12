ce6626ae714e5c72af26b9a512fc07d59dfcb4c3
Madmin <michael994664@gmail.com>
2019-05-12 17:21:38 -0400

    Add day to preprocessing - add linear regression model

--- features/bike.py
+++ features/bike.py
@@ -6,7 +6,7 @@ def trip_distance(df, year):
     key = 'trip_distance'
     if key in df.columns:
         return False
-    print("Adding bike feature " + key)
+    print("Adding bike feature: " + key)
     df[key] = zones.haversine(
     df['start_station_latitude'], 
     df['start_station_longitude'],
@@ -14,11 +14,18 @@ def trip_distance(df, year):
     df['end_station_longitude'])
     return True
 
+def day(df, year):
+    key = 'day'
+    if key in df.columns:
+        return False
+    print("Adding taxi feature: " + key)
+    df[key] = df.loc[:,'starttime'].map(lambda date: int(date.strftime('%d')))
+
 def zone_from(df, year):
     key = 'zone_from'
     if key in df.columns:
         return False
-    print("Adding bike feature " + key)
+    print("Adding bike feature: " + key)
     df[key] = np.vectorize(zones.lookup_id)(df.start_station_longitude, df.start_station_latitude)
     df.dropna(subset=[key], inplace=True)
     return True
@@ -27,7 +34,7 @@ def zone_to(df, year):
     key = 'zone_to'
     if key in df.columns:
         return False
-    print("Adding bike feature " + key)
+    print("Adding bike feature: " + key)
     df[key] = np.vectorize(zones.lookup_id)(df.end_station_longitude, df.end_station_latitude)
     df.dropna(subset=[key], inplace=True)
     return True
@@ -36,7 +43,7 @@ def zone_from_to(df, year):
     key = 'zone_from_to'
     if key in df.columns:
         return False
-    print("Adding bike feature " + key)
+    print("Adding bike feature: " + key)
     df[key] = np.vectorize(zones.from_to)(df.start_station_longitude, df.start_station_latitude, df.end_station_longitude, df.end_station_latitude)
     df.dropna(subset=[key], inplace=True)
     return True
@@ -45,6 +52,7 @@ def zone_from_to(df, year):
 def add_all_features(df, year):
     new_added = False
     new_added = trip_distance(df, year) or new_added
+    new_added = day(df, year) or new_added
     new_added = zone_from(df, year) or new_added
     new_added = zone_to(df, year) or new_added
     new_added = zone_from_to(df, year) or new_added
--- features/taxi.py
+++ features/taxi.py
@@ -5,17 +5,25 @@ def trip_duration(df, year):
     key = 'tripduration'
     if key in df.columns:
         return False
-    print("Adding taxi feature " + key)
+    print("Adding taxi feature: " + key)
     df[key] = (df['dropoff_datetime'] - df['pickup_datetime']).dt.total_seconds()
     return True
+
+def day(df, year):
+    key = 'day'
+    if key in df.columns:
+        return False
+    print("Adding taxi feature: " + key)
+    df[key] = df.loc[:,'pickup_datetime'].map(lambda date: int(date.strftime('%d')))
+
 
 def zone_from(df, year):
     key = 'zone_from'
     if key in df.columns:
         return False
     if year is '2017' or year is '2018':
-        return print("Skipping feature " + key + " for year " + year + " because of unclean data")
-    print("Adding taxi feature " + key)
+        return print("Skipping feature: " + key + " for year " + year + " because of unclean data")
+    print("Adding taxi feature: " + key)
     df[key] = np.vectorize(zones.lookup_id)(df.pickup_longitude,df.pickup_latitude)
     df.dropna(subset=[key], inplace=True)
     return True
@@ -25,8 +33,8 @@ def zone_to(df, year):
     if key in df.columns:
         return False
     if year is '2017' or year is '2018':
-        return print("Skipping feature " + key + " for year " + year + " because of unclean data")
-    print("Adding taxi feature " + key)
+        return print("Skipping feature: " + key + " for year " + year + " because of unclean data")
+    print("Adding taxi feature: " + key)
     df[key] = np.vectorize(zones.lookup_id)(df.dropoff_longitude,df.dropoff_latitude)
     df.dropna(subset=[key], inplace=True)
     return True
@@ -36,8 +44,8 @@ def zone_from_to(df, year):
     if key in df.columns:
         return False
     if year is '2017' or year is '2018':
-        return print("Skipping feature " + key + " for year " + year + " because of unclean data")
-    print("Adding taxi feature " + key)
+        return print("Skipping feature: " + key + " for year " + year + " because of unclean data")
+    print("Adding taxi feature: " + key)
     df[key] = np.vectorize(zones.from_to)(df.pickup_longitude,df.pickup_latitude,df.dropoff_longitude,df.dropoff_latitude)
     df.dropna(subset=[key], inplace=True)
     return True
@@ -45,6 +53,7 @@ def zone_from_to(df, year):
 def add_all_features(df, year):
     new_added = False
     new_added = trip_duration(df, year) or new_added
+    new_added = day(df, year) or new_added
     new_added = zone_from(df, year) or new_added
     new_added = zone_to(df, year) or new_added
     new_added = zone_from_to(df, year) or new_added
